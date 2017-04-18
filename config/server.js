539d08de41f1c086a8e058377487289f6326a8b4
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-17 22:55:32 -0300

    Configuração do bower, instalação do boostrap, angularjs.

--- config/server.js
+++ config/server.js
@@ -11,6 +11,12 @@ module.exports = function()
 {
     var app = express();
     
+    app.use(express.static('./public'));
+	app.use(express.static(__dirname + '/public'));
+
+	app.set('view engine','ejs');
+	app.set('views','./app/views');
+    
     app.set('host',process.env.IP || "127.0.0.1");
 	app.set('port',process.env.PORT || 3008);
     app.set('auth', auth);

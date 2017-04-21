3ea5a3d07a8ef0698b602fce167201c07346ac06
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-21 16:51:44 -0300

    Teste de configuração para o heroku

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,17 +1,30 @@
 var request = require("request");
 var base_url = "http://localhost:3008"
 
-describe("Teste de comunicação com a api-compromissos", function(){
+describe("Quando houver uma requisição na api compromisso", function(){
 
-  describe("GET /", function() {
+  describe("Para a rota default", function() {
     
-    it("returns status code 200", function() {
+    it("Deve retornar statusCode 200", function() {
 
-        request.get(base_url + "/api/login", function(error, response, body) {
+        request.get(base_url + "/", function(error, response, body) {
             expect(response.statusCode)
-			.toBe(200);
-            
-			done();
+			      .toBe(200);
+            done();
+        });
+
+    });
+
+  });
+
+  describe("Para uma rota desconhecida", function() {
+    
+    it("Deve retornar statusCode 404", function() {
+
+        request.get(base_url + "/base_urlsdasda/", function(error, response, body) {
+            expect(response.statusCode)
+			      .toBe(404);
+            done();
         });
 
     });

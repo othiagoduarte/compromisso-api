ed378a2888bcea962a23e083c73f8f55a5d643be
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-21 22:45:33 -0300

    Teste de publicação no heroku

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var http = require('http');
-var app = require('./config/server')();
+require('./config/server')();
 require('./config/database.js')();
-http.createServer(app).listen(app.get('port'),app.get('host'),function(){
-    console.log('Rodando na porta: ' + app.get('port'));
-	console.log('Rodando no host: ' + app.get('host'));
-});
